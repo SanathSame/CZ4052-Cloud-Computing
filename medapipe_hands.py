8abd2127b140dc74053780ef7d8afbaf15ec226b
tejas1611 <tejasgoel01@hotmail.com>
2023-04-01 16:20:10 +0800

    mediapipe hand detected frames

--- medapipe_hands.py
+++ medapipe_hands.py
@@ -2,7 +2,8 @@ import cv2
 import mediapipe as mp
 import time
 import json
-
+import os
+import traceback
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,
@@ -14,40 +15,61 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-with open("j.json", 'r') as f:
+with open("a.json", 'r') as f:
         metadata = json.load(f)
 with open("archive\\missing.txt", 'r') as f:
     missing = f.read().splitlines()
 
 for instance in metadata["instances"]:
     if instance["video_id"] not in missing:
-        id = instance["video_id"]
+        img_id = instance["video_id"]
         start = instance["frame_start"]
         end = instance["frame_end"]
         if end==-1: end = 250
         x1, y1, x2, y2 = instance["bbox"]
-        path = "archive\\videos\\" + id + ".mp4"
+        path = "archive\\videos\\" + img_id + ".mp4"
         print(path)
         cap = cv2.VideoCapture(path)
         count = 1
+        if not os.path.exists(f"archive\\frames\\{img_id}"):
+            os.makedirs(f"archive\\frames\\{img_id}")
         while True:
             try:
                 success, img = cap.read()
                 if start <= count <= end:
                     image_cropped = img[y1:y2, x1:x2]
+                    h, w, c = image_cropped.shape
                     # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                     results = hands.process(image_cropped)
                     # print(results.multi_hand_landmarks)
                     if results.multi_hand_landmarks:
+                        x_max = 0
+                        y_max = 0
+                        x_min = w
+                        y_min = h
                         for handLms in results.multi_hand_landmarks:
                             for id, lm in enumerate(handLms.landmark):
                                 #print(id,lm)
-                                h, w, c = image_cropped.shape
                                 cx, cy = int(lm.x *w), int(lm.y*h)
                                 #if id ==0:
                                 cv2.circle(image_cropped, (cx,cy), 3, (255,0,255), cv2.FILLED)
+                                x_max = max(cx, x_max)
+                                y_max = max(cy, y_max)
+                                x_min = min(cx, x_min)
+                                y_min = min(cy, y_min)
 
                             mpDraw.draw_landmarks(image_cropped, handLms, mpHands.HAND_CONNECTIONS)
+                        
+                        cv2.rectangle(image_cropped, (x_min-10, y_min-10), (x_max+10, y_max+10), (0, 255, 0), 2)
+                            
+                        try:
+                            image_save = image_cropped[y_min-10:y_max+10, x_min-10:x_max+10]
+                            cv2.imshow("hand", image_save)
+                            if not cv2.imwrite(f"archive\\frames\\{img_id}\\{count}.jpg", image_save):
+                                print(f"archive\\frames\\{id}\\{count}{time.time()}.jpg")
+                                print("Not saved")
+                        except:
+                            print("Nothing")
 
                     cTime = time.time()
                     fps = 1/(cTime-pTime)
@@ -58,5 +80,6 @@ for instance in metadata["instances"]:
                     cv2.imshow("Detected hands", image_cropped)
                     cv2.waitKey(1)
                 count += 1
-            except:
+            except Exception:
+                print(traceback.format_exc())
                 break
