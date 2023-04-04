29b35bd26eae7f25889ea18d9df641a20c5d782a
Sdidt <siddharthms607@gmail.com>
2023-04-04 20:14:55 +0800

    feat: added file upload functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,35 @@
-import { Amplify } from "aws-amplify";
+import { Amplify, Storage } from "aws-amplify";
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import "./ui-components/index";
 
 import awsExports from "./aws-exports";
+import { useState } from "react";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus] = useState(false);
+
+  const uploadFile = async () => {
+    const result = await Storage.put(fileData.name, fileData, {
+      contentType: fileData.type,
+    });
+    setFileStatus(true);
+    console.log(result);
+  };
+
   return (
     <>
       <h1>Hello {user.username}</h1>
+      <div>
+        <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
+      </div>
+      <div>
+        <button onClick={uploadFile}>Upload file</button>
+      </div>
+      {fileStatus ? "File uploaded successfully" : ""}
       <button onClick={signOut}>Sign out</button>
     </>
   );
