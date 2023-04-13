749ac4ae8fcade31536cb9379a7fa8c6b5b0ef02
Sdidt <siddharthms607@gmail.com>
2023-04-13 18:03:21 +0800

    dummy commit

--- frontend/src/Components/allcourses/CoursesCard.jsx
+++ frontend/src/Components/allcourses/CoursesCard.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import { coursesCard } from '../../dummydata'
-import './courses.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { coursesCard } from "../../dummydata";
+import "./courses.css";
+import { Link } from "react-router-dom";
 
 const CoursesCard = () => {
-    return (
-        <>
-            <section className="coursesCard">
-                <div className="container grid2">
-                    {coursesCard.map((val) => {
-                        return (
-                            <div className="items">
-                                <div className="content flex">
-                                    <div className="left">
-                                        <div className="img">
-                                            <img src={val.cover} alt="" />
-                                        </div>
-                                    </div>
-                                    <div className="text">
-                                        <h1>{val.coursesName}</h1>
-                                    </div>
-                                </div>
-                                <div className="price">
-                                    <img src='/public/images/Course1.jpg' alt='Letter' />
-                                </div>
-                                <Link to='/lesson'>
-                                    <button className="outline-btn">LEARN</button>
-                                </Link>
-                            </div>
-                        )
-                    })}
+  return (
+    <>
+      <section className="coursesCard">
+        <div className="container grid2">
+          {coursesCard.map((val) => {
+            return (
+              <div className="items">
+                <div className="content flex">
+                  <div className="left">
+                    <div className="img">
+                      <img src={val.cover} alt="" />
+                    </div>
+                  </div>
+                  <div className="text">
+                    <h1>{val.coursesName}</h1>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
-}
+                <div className="price">
+                  <img src="/public/images/Course1.jpg" alt="Letter" />
+                </div>
+                <Link to="/lesson">
+                  <button className="outline-btn">LEARN</button>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default CoursesCard
+export default CoursesCard;
